ebbb50d0701471814faca3c55ee69797c9ca0c0c
SBKubric <sbkubric@gmail.com>
2017-01-12 16:40:16 +0300

    Udpated README.md, minor fixes

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 ## Description
 
-There should be a description.
+The script helps you to track your sites. It takes a path to the file with links to sources and returns if its response
+code is 200 and if domain is paid for next month.
 
 ##Installation
 
@@ -16,19 +17,56 @@ pip install -r requirements.txt
 
 **IMPORTANT**: best practice is to use virtualenv. See here: [Link](http://docs.python-guide.org/en/latest/dev/virtualenvs/)
 
+##File format
+
+The line format in file should be like this: `schema:url\n`.
+
+For example:
+
+**FILE:**
+
+```
+http://www.google.com
+http://www.yandex.com
+http://www.rambler.com
+http://www.azaza.ru
+```
+
 ##Example
 
 **INPUT:**
 
 ```
-python3 check_sites_health.py
+python3 check_sites_health.py urls -o result
 ```
 
 **OUTPUT:**
 
+It will be located at `./result.log`
+
 ```
-There should be output
+http://www.google.com
+  response_code_200: True
+  is_paid_for_next_month: True
+
+
+http://www.yandex.com
+  response_code_200: True
+  is_paid_for_next_month: True
+
+
+http://www.rambler.com
+  response_code_200: True
+  is_paid_for_next_month: True
+
+
+http://www.azaza.ru
+  response_code_200: True
+  is_paid_for_next_month: True
+
 ```
+
+Use `-h` or `--help` argument for HELP.
 
 # Project Goals
 
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,13 +1,31 @@
 import pythonwhois
 import requests
 import argparse
-from datetime import datetime, timedelta
+from datetime import datetime
+from calendar import monthrange
 import tldextract
 import logging
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='')
+    parser = argparse.ArgumentParser(description='SMUS (Sites Monitoring Utility Script)\n'
+                                                 'The script helps you to control your sites.'
+                                                 'It can return their if their response is 200 and check their'
+                                                 'domain expiration date.')
+    parser.add_argument('source', help='The path to the url list file')
+    parser.add_argument('-nr', '--no_redirects', action='store_true', help='Forbid redirection from sites')
+    parser.add_argument('-o', '--output', default='logfile', help='The name of file with results')
+    args = parser.parse_args()
+    return args
+
+
+def get_logger(logfile_path):
+    logging.basicConfig(format='%(message)s', filename='{}.log'.format(logfile_path), level=logging.INFO)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    logger = logging.getLogger()
+    logger.addHandler(ch)
+    return logger
 
 
 def load_urls4check(path):
@@ -16,11 +34,13 @@ def load_urls4check(path):
         url = file.readline()
         if not url:
             break
+        if '\n' in url:
+            url = url[:-1]
         yield url
 
 
-def is_server_respond_with_200(url):
-    response = requests.get(url)
+def is_server_respond_with_200(url, forbid_redirects=False):
+    response = requests.get(url, allow_redirects=not forbid_redirects)
     if response.status_code == 200:
         return True
     return False
@@ -32,25 +52,30 @@ def get_domain_expiration_date(domain_name):
 
 
 def get_major_domain(url):
-    tld_info = tldextract.extract('www.rambler.ru')
+    tld_info = tldextract.extract(url)
     domain = '{}.{}'.format(tld_info.domain, tld_info.suffix)
     return domain
 
 
 def is_paid_for_next_month(url):
     domain = get_major_domain(url)
-    exp_date = get_domain_expiration_date(domain)
+    dt_exp = get_domain_expiration_date(domain)
     now_date = datetime.now()
-    if exp_date - now_date <= timedelta(months=1):
+    if now_date.month == 12:
+        due_date = datetime(now_date.year+1, 1, 31, 23, 0, 0)
+    else:
+        first_day, last_day = monthrange(now_date.year, now_date.month+1)
+        due_date = datetime(now_date.year, now_date.month+1, last_day, 23, 0, 0)
+    if dt_exp < due_date:
         return False
     return True
 
 
 if __name__ == '__main__':
     args = parse_args()
-    logger = logging.getLogger()
-    for url in load_urls4check(args.path):
+    logger = get_logger(args.output)
+    for url in load_urls4check(args.source):
         logger.info('{}\n  response_code_200: {}\n  is_paid_for_next_month: {}\n\n'.format(
-            url, is_server_respond_with_200(url), is_paid_for_next_month(url)
+            url, is_server_respond_with_200(url, args.no_redirects), is_paid_for_next_month(url)
         ))
 
